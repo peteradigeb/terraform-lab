49d0cee42ba6e0428a9e78f2d132160a1c08eeda
vboxuser <vboxuser@cccc.myguest.virtualbox.org>
2026-01-03 19:18:09 +0100

    aws ec2 instances using terraform

---
+++ README.md
@@ -0,0 +1,6 @@
+In this project i created two EC2 instance using terraform
+
+
+
+-created security group for http and ssh
+-rovision of two EC2 instances
---
+++ main.tf
@@ -0,0 +1,43 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_security_group" "web_sg" {
+  name        = "web-sg"
+  description = "Allow SSH and HTTP"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["${var.my_ip}/32"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "web" {
+  count         = 2
+  ami           = "ami-0ecb62995f68bb549"
+  instance_type = "t3.micro"
+  key_name      = "anss"
+
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+
+  tags = {
+    Name = "web-${count.index}"
+  }
+}
+
